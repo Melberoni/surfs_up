42a99e1a42244263001a7cad7914e2af761ac5ce
Robertson <Robertson.Melissa1@mayo.edu>
2021-11-17 21:38:10 -0600

    added precip route

--- app.py
+++ app.py
@@ -1,8 +1,44 @@
-from flask import Flask
+#dependencies 
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+#sql light engine
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+#define our app for flask
 
 app= Flask(__name__)
 @app.route('/')
 
-def hello_world():
-    return 'Hello world'
+def welcome():
+    return(
+        """
+            Welcome to the climate Analysis API! <br>
+            Available Routes: <br>
+            /api/v1.0/precipitation <br>
+            /api/v1.0/stations <br>
+            /api/v1.0/tobs <br>
+            /api/v1.0/temp/start/end <br>
+        """)
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
